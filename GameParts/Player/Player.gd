903c7b5dc3a42eb32f9d2dbe62861e12cd39e7c6
Unknown <ktoso3@gmail.com>
2019-10-12 18:55:20 +0200

    adding shaders

--- GameParts/Player/Player.gd
+++ GameParts/Player/Player.gd
@@ -3,16 +3,24 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var speed = 30
+var speed = 3
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+	var direction = Vector2()
+	if(Input.is_action_pressed("ui_down")):
+		direction += Vector2(0,1)
+	if(Input.is_action_pressed("ui_up")):
+		direction += Vector2(0,-1)
+	if(Input.is_action_pressed("ui_left")):
+		direction += Vector2(-1,0)
+	if(Input.is_action_pressed("ui_right")):
+		direction += Vector2(1,0)
+	move_and_collide(direction * speed)
 	pass
